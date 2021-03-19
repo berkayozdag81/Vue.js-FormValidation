7bf521fc211cdf9ca492174656d8bb811ed830b4
Berkay Özdağ <berkayozdag2001@gmail.com>
2021-03-19 18:07:31 +0300

    no message

--- form-validation/src/FormValidation.vue
+++ form-validation/src/FormValidation.vue
@@ -1,43 +1,56 @@
 <template>
   <div class="card">
-    <h3 class="card-header text-center">Kayıt Formu </h3>
+    <h3 class="card-header text-center">Vue Form Oluşturma </h3>
     <div class="card-body">
       <form>
         <div class="form-row">
           <div class="form-group col-md-6">
-            <label>First name : </label>
-            <input type="text" class="form-control" v-model.trim="$v.firstname.$model"
-                   :class="{'is-invalid' : $v.firstname.$error, 'is-valid':!$v.firstname.$invalid}">
-            <div class="valid-feedback">Your firstname is valid!</div>
+            <label>Slider type : </label>
+            <input type="text" class="form-control" v-model.trim="$v.sliderType.$model"
+                   :class="{'is-invalid' : $v.sliderType.$error, 'is-valid':!$v.sliderType.$invalid}">
+            <div class="valid-feedback">Slider type is valid!</div>
             <div class="invalid-feedback">
-              <span v-if="!$v.firstname.required">Firstname is required</span>
-              <span v-if="!$v.firstname.minLength">First name must have at least
-                {{$v.firstname.$params.minLength.min}} letters</span>
-              <span v-if="!$v.firstname.maxLength">First name must have at least
-                {{$v.firstname.$params.maxLength.max}} letters</span>
+              <span v-if="!$v.sliderType.required">Slider type is required</span>
+              <span v-if="!$v.sliderType.minLength">Slider type must have at least
+                {{$v.sliderType.$params.minLength.min}} letters</span>
+              <span v-if="!$v.sliderType.maxLength">Slider type must have at least
+                {{$v.sliderType.$params.maxLength.max}} letters</span>
             </div>
           </div>
           <div class="form-group col-md-6">
-            <label>Last name : </label>
-            <input type="text" class="form-control" v-model.trim="$v.lastname.$model"
-                   :class="{'is-invalid' : $v.lastname.$error, 'is-valid':!$v.lastname.$invalid}">
-            <div class="valid-feedback">Your lastname is valid!</div>
+            <label>createdDate : </label>
+            <input type="text" class="form-control" v-model.trim="$v.createdDate.$model"
+                   :class="{'is-invalid' : $v.createdDate.$error, 'is-valid':!$v.createdDate.$invalid}">
+            <div class="valid-feedback">createdDate is valid!</div>
             <div class="invalid-feedback">
-              <span v-if="!$v.lastname.required">lastname is required</span>
-              <span v-if="!$v.lastname.minLength">lastname name must have at least
-                {{$v.lastname.$params.minLength.min}} letters</span>
-              <span v-if="!$v.lastname.maxLength">lastname name must have at least
-                {{$v.lastname.$params.maxLength.max}} letters</span>
+              <span v-if="!$v.createdDate.required">createdDate is required</span>
+              <span v-if="!$v.createdDate.minLength">createdDate name must have at least
+                {{$v.createdDate.$params.minLength.min}} letters</span>
+              <span v-if="!$v.createdDate.maxLength">createdDate name must have at least
+                {{$v.createdDate.$params.maxLength.max}} letters</span>
             </div>
           </div>
-          <div class="form-group">
-            <label>Age : </label>
-            <input type="number" class="form-control" v-model.number.lazy="$v.age.$model"
-                   :class="{'is-invalid' : $v.age.$error, 'is-valid':!$v.age.$invalid}">
-            <div class="valid-feedback">age is valid!</div>
+          <div class="form-group col-md-6">
+            <label>Project ID : </label>
+            <input type="number" class="form-control" v-model.number.lazy="$v.projectID.$model"
+                   :class="{'is-invalid' : $v.projectID.$error, 'is-valid':!$v.projectID.$invalid}">
+            <div class="valid-feedback">projectID is valid!</div>
             <div class="invalid-feedback">
-              <span v-if="!$v.age.between">age must be between {{$v.age.$params.between.min}} and
-                {{$v.age.$params.between.max}}</span>
+              <span v-if="!$v.projectID.between">projectID must be between {{$v.projectID.$params.between.min}} and
+                {{$v.projectID.$params.between.max}}</span>
+            </div>
+          </div>
+          <div class="form-group col-md-6">
+            <label>description : </label>
+            <input type="text" class="form-control" v-model.trim="$v.description.$model"
+                   :class="{'is-invalid' : $v.description.$error, 'is-valid':!$v.description.$invalid}">
+            <div class="valid-feedback">description is valid!</div>
+            <div class="invalid-feedback">
+              <span v-if="!$v.description.required">description is required</span>
+              <span v-if="!$v.description.minLength">description name must have at least
+                {{$v.description.$params.minLength.min}} letters</span>
+              <span v-if="!$v.description.maxLength">description name must have at least
+                {{$v.description.$params.maxLength.max}} letters</span>
             </div>
           </div>
         </div>
@@ -45,7 +58,6 @@
     </div>
   </div>
 </template>
-
 <script>
 import { required, minLength, between, maxLength } from 'vuelidate/lib/validators'
 
@@ -53,24 +65,30 @@ export default {
   name : 'Form Validation',
   data(){
     return {
-      firstname : '',
-      lastname : '',
-      age : 0
+      sliderType : '',
+      projectID : 0,
+      description : '',
+      createdDate: ''
     }
   },
   validations: {
-    firstname : {
+    sliderType : {
       required,
       minLength: minLength(3),
       maxLength: maxLength(10),
     },
-    lastname : {
+    description : {
       required,
       minLength: minLength(3),
       maxLength: maxLength(10),
     },
-    age :{
+    projectID :{
       between:between(15,40)
+    },
+    createdDate :{
+      required,
+      minLength: minLength(3),
+      maxLength: maxLength(10),
     }
   }
 }
